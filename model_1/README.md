ba9d1d433e77962422424159bcdefc297dd02512
shrestha-pranav <shresthapranav278@gmail.com>
2018-11-16 05:12:10 -0500

    Updated model to reach 69.8%

--- model_1/README.md
+++ model_1/README.md
@@ -1,1 +1,8 @@
+| Model \ Dataset                                    | cb513 |
+| -------------------------------------------------- | ----- |
+| Model 1: Convolutions with Bidirectional GRU | 69.8% |
 
+| Author     | Affiliation         | Email               |
+| ---------- | ------------------- | ------------------- |
+| Pranav Shrestha | Columbia University | ps2958@columbia.edu |
+| Jeffrey Wan  | Columbia University | jw3468@columbia.edu |
--- model_1/model_1.py
+++ model_1/model_1.py
@@ -34,7 +34,7 @@ print()
 print(cb6133filtered.shape)
 print(cb513.shape)
 
-r = 700 # protein residues padded to 700
+maxlen_seq = r = 700 # protein residues padded to 700
 f = 57  # number of features for each residue
 
 residue_list = list('ACEDGFIHKMLNQPSRTWVYX') + ['NoSeq']
@@ -77,9 +77,6 @@ def get_data(arr, bounds=None):
 ### Train-test Specification
 train_df = get_data(cb6133filtered)
 test_df  = get_data(cb513)
-val_df   = test_df # get_data(cb513)
-
-maxlen_seq = 700
 
 # The custom accuracy metric used for this task
 def accuracy(y_true, y_pred):
@@ -105,10 +102,6 @@ def seq2onehot(seq, n):
         for j in range(maxlen_seq):
             out[i, j, seq[i, j]] = 1
     return out
-
-# prints the results
-def print_results(x, y_, revsere_decoder_index):
-    print(str(onehot_to_seq(y_, revsere_decoder_index).upper()))
 
 # Computes and returns the n-grams of a particualr sequence, defaults to trigrams
 def seq2ngrams(seqs, n = 1):
@@ -141,19 +134,6 @@ train_target_data = sequence.pad_sequences(train_target_data,
                                            maxlen = maxlen_seq, padding='post')
 train_target_data = to_categorical(train_target_data)
 
-# Tokenize Validation
-# Same for val
-val_input_seqs, val_target_seqs = val_df[['input', 'expected']][(val_df.len.astype(int) <= maxlen_seq)].values.T
-val_input_grams = seq2ngrams(val_input_seqs)
-
-# Inputs
-val_input_data = tokenizer_encoder.texts_to_sequences(val_input_grams)
-val_input_data = sequence.pad_sequences(val_input_data, maxlen = maxlen_seq, padding = 'post')
-# Targets
-val_target_data = tokenizer_decoder.texts_to_sequences(val_target_seqs)
-val_target_data = sequence.pad_sequences(val_target_data, maxlen = maxlen_seq, padding = 'post')
-val_target_data = to_categorical(val_target_data)
-
 # Use the same tokenizer defined on train for tokenization of test
 test_input_data = tokenizer_encoder.texts_to_sequences(test_input_grams)
 test_input_data = sequence.pad_sequences(test_input_data,
@@ -171,10 +151,6 @@ test_input_data_alt = test_input_data
 test_input_data = seq2onehot(test_input_data, n_words)
 test_profiles = test_df.profiles.values
 
-val_input_data_alt = val_input_data
-val_input_data = seq2onehot(val_input_data, n_words)
-val_profiles = val_df.profiles.values
-
 train_profiles_np = np.zeros((len(train_profiles), maxlen_seq, 22))
 for i, profile in enumerate(train_profiles):
     for j in range(profile.shape[0]):
@@ -187,80 +163,20 @@ for i, profile in enumerate(test_profiles):
         for k in range(profile.shape[1]):
             test_profiles_np[i, j, k] = profile[j, k]
 
-val_profiles_np = np.zeros((len(val_profiles), maxlen_seq, 22))
-for i, profile in enumerate(val_profiles):
-    for j in range(profile.shape[0]):
-        for k in range(profile.shape[1]):
-            val_profiles_np[i, j, k] = profile[j, k]
-
-""" Build model """
-def conv_block(x, activation=True, batch_norm=True, drop_out=True, res=True):
-    cnn = Conv1D(64, 11, padding="same")(x)
-    if activation: cnn = TimeDistributed(Activation("relu"))(cnn)
-    if batch_norm: cnn = TimeDistributed(BatchNormalization())(cnn)
-    if drop_out:   cnn = TimeDistributed(Dropout(0.5))(cnn)
-    if res:        cnn = Concatenate(axis=-1)([x, cnn])
-    
-    return cnn
-
-def super_conv_block(x):
-    c3 = Conv1D(32, 3, padding="same")(x)
-    c3 = TimeDistributed(Activation("relu"))(c3)
-    c3 = TimeDistributed(BatchNormalization())(c3)
-    c3 = TimeDistributed(Dropout(0.5))(c3)
-    
-    c7 = Conv1D(64, 7, padding="same")(x)
-    c7 = TimeDistributed(Activation("relu"))(c7)
-    c7 = TimeDistributed(BatchNormalization())(c7)
-    c7 = TimeDistributed(Dropout(0.5))(c7)
-    
-    c11 = Conv1D(128, 11, padding="same")(x)
-    c11 = TimeDistributed(Activation("relu"))(c11)
-    c11 = TimeDistributed(BatchNormalization())(c11)
-    c11 = TimeDistributed(Dropout(0.5))(c11)
-    
-    x = Concatenate(axis=-1)([x, c3, c7, c11])
-    return x
-
-def CNN_BIGRU():
-    # inp is one-hot encoded, inp_alt is the non one-hot encoded sequence
-    inp          = Input(shape=(maxlen_seq, n_words))
-    inp_alt      = Input(shape=(maxlen_seq,))
-    inp_profiles = Input(shape=(maxlen_seq, 22))
-
-    #Concatenate embedded and unembedded input
-    x_emb = Embedding(input_dim=n_words, output_dim=64, 
-                      input_length=maxlen_seq)(inp_alt)
-    x = Concatenate(axis=-1)([inp, x_emb, inp_profiles])
-
-    x = super_conv_block(x)
-    x = conv_block(x)
-    x = conv_block(x)
-    x = conv_block(x)
-
-    x = Bidirectional(CuDNNGRU(units = 256, return_sequences = True, recurrent_regularizer=l2(0.2)))(x)
-    x = TimeDistributed(Dense(128, activation = "relu"))(x)
-    x = TimeDistributed(Dense(64, activation = "relu"))(x)
-    y = TimeDistributed(Dense(n_tags, activation = "softmax"))(x)
-
-    model = Model([inp, inp_alt, inp_profiles], y)
-    
-    return model
-
-def decode_results(x, y_, revsere_decoder_index):
-    print("prediction: " + str(onehot_to_seq(y_, revsere_decoder_index).upper()))
-    return str(onehot_to_seq(y_, revsere_decoder_index).upper())
+def decode_results(y_, reverse_decoder_index):
+    print("prediction: " + str(onehot_to_seq(y_, reverse_decoder_index).upper()))
+    return str(onehot_to_seq(y_, reverse_decoder_index).upper())
 
 def run_test(_model, data1, data2, data3, csv_name, npy_name):
-    revsere_decoder_index = {value:key for key,value in tokenizer_decoder.word_index.items()}
-    revsere_encoder_index = {value:key for key,value in tokenizer_encoder.word_index.items()}
+    reverse_decoder_index = {value:key for key,value in tokenizer_decoder.word_index.items()}
+    reverse_encoder_index = {value:key for key,value in tokenizer_encoder.word_index.items()}
     
     # Get predictions using our model
     y_test_pred = _model.predict([data1, data2, data3])
 
     decoded_y_pred = []
     for i in range(len(test_input_data)):
-        res = decode_results(test_input_seqs[i], y_test_pred[i], revsere_decoder_index)
+        res = decode_results(y_test_pred[i], reverse_decoder_index)
         decoded_y_pred.append(res)
 
     # Set Columns
@@ -276,7 +192,7 @@ def run_test(_model, data1, data2, data3, csv_name, npy_name):
 
 
 """ Run below for a single run """
-def train(X_train, y_train, X_val, y_val):
+def train(X_train, y_train, X_val=None, y_val=None):
     """
     Main Training function with the following properties:
         Optimizer - Nadam
@@ -290,26 +206,76 @@ def train(X_train, y_train, X_val, y_val):
         loss = "categorical_crossentropy",
         metrics = ["accuracy", accuracy])
     
-    history = model.fit(
-        X_train,
-        y_train,
-        batch_size = 128,
-        epochs = 50,
-        validation_data = (X_val, y_val)
-    )
+    if X_val is not None and y_val is not None:
+        history = model.fit( X_train, y_train,
+            batch_size = 128, epochs = 100,
+            validation_data = (X_val, y_val))
+    else:
+        history = model.fit( X_train, y_train,
+            batch_size = 128, epochs = 100)
 
     return history, model
+
+""" Build model """
+def conv_block(x, activation=True, batch_norm=True, drop_out=True, res=True):
+    cnn = Conv1D(64, 11, padding="same")(x)
+    if activation: cnn = TimeDistributed(Activation("relu"))(cnn)
+    if batch_norm: cnn = TimeDistributed(BatchNormalization())(cnn)
+    if drop_out:   cnn = TimeDistributed(Dropout(0.5))(cnn)
+    if res:        cnn = Concatenate(axis=-1)([x, cnn])
+    
+    return cnn
+
+def super_conv_block(x):
+    c3 = Conv1D(32, 1, padding="same")(x)
+    c3 = TimeDistributed(Activation("relu"))(c3)
+    c3 = TimeDistributed(BatchNormalization())(c3)
+    
+    c7 = Conv1D(64, 3, padding="same")(x)
+    c7 = TimeDistributed(Activation("relu"))(c7)
+    c7 = TimeDistributed(BatchNormalization())(c7)
+    
+    c11 = Conv1D(128, 5, padding="same")(x)
+    c11 = TimeDistributed(Activation("relu"))(c11)
+    c11 = TimeDistributed(BatchNormalization())(c11)
+    
+    x = Concatenate(axis=-1)([x, c3, c7, c11])
+    x = TimeDistributed(Dropout(0.5))(x)
+    return x
+
+def CNN_BIGRU():
+    # Inp is one-hot encoded version of inp_alt
+    inp          = Input(shape=(maxlen_seq, n_words))
+    inp_alt      = Input(shape=(maxlen_seq,))
+    inp_profiles = Input(shape=(maxlen_seq, 22))
+
+    # Concatenate embedded and unembedded input
+    x_emb = Embedding(input_dim=n_words, output_dim=64, 
+                      input_length=maxlen_seq)(inp_alt)
+    x = Concatenate(axis=-1)([inp, x_emb, inp_profiles])
+
+    x = super_conv_block(x)
+    x = conv_block(x)
+    x = super_conv_block(x)
+    x = conv_block(x)
+    x = super_conv_block(x)
+    x = conv_block(x)
+
+    x = Bidirectional(CuDNNGRU(units = 256, return_sequences = True, recurrent_regularizer=l2(0.2)))(x)
+    x = TimeDistributed(Dropout(0.5))(x)
+    x = TimeDistributed(Dense(256, activation = "relu"))(x)
+    x = TimeDistributed(Dropout(0.5))(x)
+    
+    y = TimeDistributed(Dense(n_tags, activation = "softmax"))(x)
+    
+    model = Model([inp, inp_alt, inp_profiles], y)
+    
+    return model
 
 X_train = [train_input_data, train_input_data_alt, train_profiles_np]
 y_train = train_target_data
 
-X_val   = [val_input_data, val_input_data_alt, val_profiles_np]
-y_val   = val_target_data
-
-history, model = train(X_train, y_train, X_val, y_val)
-
-# Save keras model
-model.save("model_1.pkl")
+history, model = train(X_train, y_train)
 
 # Save the model as a JSON format
 model.save_weights("cb513_weights_1.h5")
@@ -320,7 +286,7 @@ with open("model_1.json", "w") as json_file:
 with open("history_1.pkl", "wb") as hist_file:
     pickle.dump(history.history, hist_file)
 
-# Run accuracy test on test dataset and save the output
+# Predict on test dataset and save the output
 run_test(model,
     test_input_data[:],
     test_input_data_alt[:],
